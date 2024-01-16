0e22996836036f28f4e90b163024bd99ee6a9316
Adwall28 <98455674+Adwall28@users.noreply.github.com>
2024-01-16 12:10:53 +0000

    Update2

---
+++ Logic.js
@@ -0,0 +1,8 @@
+
+
+let SearchInput = document.getElementById("SearchInput");
+let SearchButton = document.getElementById("SearchButton")
+
+SearchButton.onclick = function(){
+   SearchInput.innerHTML = ""; 
+}
--- Server.js
+++ Server.js
@@ -4,9 +4,14 @@ let fs = require('fs');
 let port = 8080;
 let url = require("url");
 const {MongoClient} = require('mongodb');
+import mongoose from 'mongoose';
+import Book from './model/Book';
 let express = require("express");
 let app = express();
 let path = require("path");
+let $ = require('jquery');
+
+mongoose.connect("mongodb+srv://adamjohnwallace1:FanAppa123@library.4yodunz.mongodb.net/?retryWrites=true&w=majority");
 
 
 async function main() {
@@ -23,7 +28,13 @@ async function main() {
     } finally {
         await client.close();
     }
+
+    const db = client.db("Library");
+    const Books = db.collection("Books");
+    const Users = db.collection("Users");
 }
+
+
 
 main().catch(console.error);
 
@@ -33,12 +44,13 @@ async function listDatabases(client){
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
  
 app.set("view engine", "ejs");
 app.use(express.static(__dirname));
 
 app.get("/", function(request, response){
-    response.render("pages/Login");
+    response.render("pages/Index");
 })
 
 app.listen(port, function(){

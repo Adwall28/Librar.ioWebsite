a8a1a7d78c6587507ea0b0005ba3dc9872d577d3
Adwall28 <98455674+Adwall28@users.noreply.github.com>
2024-01-16 11:27:05 +0000

    Update1

--- Server.js
+++ Server.js
@@ -4,15 +4,12 @@ let fs = require('fs');
 let port = 8080;
 let url = require("url");
 const {MongoClient} = require('mongodb');
-const mongoose = require("mongoose");
 let express = require("express");
 let app = express();
 let path = require("path");
 
-mongoose.set("strictQuery", false);
 
-
-/*async function main() {
+async function main() {
 	let uri = "mongodb+srv://adamjohnwallace1:FanAppa123@library.4yodunz.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
 
@@ -26,23 +23,16 @@ mongoose.set("strictQuery", false);
     } finally {
         await client.close();
     }
-}*/
-
-const mongoDB = "mongodb+srv://adamjohnwallace1:FanAppa123@library.4yodunz.mongodb.net/?retryWrites=true&w=majority"
-
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
 }
 
-/*main().catch(console.error);
+main().catch(console.error);
 
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
  
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};*/
+};
  
 app.set("view engine", "ejs");
 app.use(express.static(__dirname));
